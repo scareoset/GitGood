d6f15160cd94f5bcd70c4e809d6da0893d82427c
scareoset <daniel.wright.314@gmail.com>
2019-05-07 19:54:40 -0700

    branch test
    please work

--- GitGood.java
+++ GitGood.java
@@ -1,5 +1,9 @@
 public class GitGood{
 	public static void main(String[] args) {
-		System.out.prinlt("git good noob");
+		System.out.prinlt(message());
+	}
+
+	public static String message() {
+		return "git good noob";
 	}
 }
